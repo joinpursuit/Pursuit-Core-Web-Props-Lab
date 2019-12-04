656ea5bf6dccad1083f980097c7f5485575dcfcb
Bpmauge <briahanamauge@pursuit.org>
2019-12-04 12:10:04 -0500

    tightening up the past donations side & working on making a donation

--- go-fund-me/src/App.js
+++ go-fund-me/src/App.js
@@ -10,19 +10,40 @@ function App() {
         <div className='header'>
 
           <div className='header-text'>
-            <h1>GO FUND <em>ME</em></h1>
+            <h1>GO FUND <div id='me'>ME</div></h1>
             <p>Help me go on a vacation somewhere</p>
           </div>
 
         </div>
+        <div className='body'>
 
           <div className='body-left'>
-          <p>This is the body left.</p>
+            <h6>Recent Donations</h6>
+            <ul>
+              <li className='donation'>___ donated some money
+              <p className='comment'>do you love me</p>
+              </li>
+              <li className='donation'>___ donated some money
+              <p className='comment'>do you love me</p>
+              </li><
+                li className='donation'>___ donated some money
+              <p className='comment'>do you love me</p>
+              </li>
+              <li className='donation'>___ donated some money
+              <p className='comment'>do you love me</p>
+              </li>
+            </ul>
           </div>
 
           <div className='body-right'>
-            <p>This is the body right.</p>
+            <div id='goal'>
+              <h4>Raised $441 of <div id='total'>$1000</div> </h4>
+              <div id = 'progress-bar'>100%</div>
+            </div>
+
           </div>
+
+        </div>
 
       </div>
 
