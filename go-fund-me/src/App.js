9c5bc87131db2d7e4c334a91b51ac311fe97963d
Bpmauge <briahanamauge@pursuit.org>
2019-12-04 10:31:24 -0500

    skeleton of the app parts

---
+++ go-fund-me/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className='main-page'>
+
+        <div className='header'>
+
+          <div className='header-text'>
+            <h1>GO FUND <em>ME</em></h1>
+            <p>Help me go on a vacation somewhere</p>
+          </div>
+
+        </div>
+
+          <div className='body-left'>
+          <p>This is the body left.</p>
+          </div>
+
+          <div className='body-right'>
+            <p>This is the body right.</p>
+          </div>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
