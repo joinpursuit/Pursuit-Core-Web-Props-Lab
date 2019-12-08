9e4d3d0bb279a134f1859f1795306dac813c2391
Bpmauge <briahanamauge@pursuit.org>
2019-12-08 18:06:28 -0500

    finished lab

--- go-fund-me/src/App.js
+++ go-fund-me/src/App.js
@@ -1,65 +1,105 @@
 import React from 'react';
 import './App.css';
+import DonationForm from './Components/donationForm';
+import Progress from './Components/progress';
+import RecentDonations from './Components/recentDonations';
+import TopBar from './Components/TopBar';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      goalAmount: 1000,
+      raised: 0,
+      donations: [],
+      donatedAmount: 5,
+      donorName: '',
+      donorCaption: '',
+      percentage: 0
+    }
+  }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { goalAmount, raised, donatedAmount, donorCaption, donorName, donations } = this.state;
+    let newDonationCopy = [...donations];
 
-      <div className='main-page'>
+    newDonationCopy.push({
+      name: donorName,
+      caption: donorCaption,
+      amount: donatedAmount
+    })
 
-        <div className='header'>
+    this.setState({
+      donations: newDonationCopy,
+      raised: raised + donatedAmount,
+      percentage: ((raised + donatedAmount) / goalAmount) * 100
+    })
 
-          <div className='header-text'>
-            <h1>GO FUND <div id='me'>ME</div></h1>
-            <p>Help me go on a vacation somewhere</p>
-          </div>
 
-        </div>
-        <div className='body'>
+  }
+  handleChangeDonatedAmount = (event) => {
+    this.setState({
+      donatedAmount: parseInt(event.target.value)
+    })
+  }
+  handleDonorName = (event) => {
+    this.setState({
+      donorName: event.target.value
+    })
+  }
 
-          <div className='body-left'>
-            <h6>Recent Donations</h6>
-            <ul>
-              <li className='donation'>___ donated some money
-              <p className='comment'>do you love me</p>
-              </li>
-              <li className='donation'>___ donated some money
-              <p className='comment'>do you love me</p>
-              </li><
-                li className='donation'>___ donated some money
-              <p className='comment'>do you love me</p>
-              </li>
-              <li className='donation'>___ donated some money
-              <p className='comment'>do you love me</p>
-              </li>
-            </ul>
-          </div>
+  handleDonorCaption = (event) => {
+    this.setState({
+      donorCaption: event.target.value
+    })
+  }
+  render() {
+    let { raised, donatedAmount, donorName, donorCaption, donations, percentage } = this.state;
+    return (
+      <div className="App">
 
-          <div className='body-right'>
-            <div id='goal'>
-              <h4>Raised $441 of <div id='total'>$1000</div> </h4>
-              <div id='progress-bar'>100%</div>
+        <div className='main-page'>
+          <TopBar />
+          <div className='body'>
+
+            <div className='body-left'>
+              <RecentDonations donations={donations} />
             </div>
 
-            <div id='donation-stage'>
-              <form className='donation-form'>
-                <p className='label' htmlFor='name'>Name <br></br>
-                  <input type='text' placeholder='John Doe' id='input-name' /></p> <br></br>
-                <p className='label' htmlFor='comment'>Comment <br></br>
-                  <input type='text' placeholder='. . .' id='input-comment' /></p>
-                <br></br>
-                <button>Donate</button>
-              </form>
-            </div>
 
+            <div className='body-right'>
+              <div id='goal'>
+               
+                <Progress
+                raised = {raised}
+                  percentage={percentage}
+                />
+
+              </div>
+
+              <DonationForm
+                newDonation={this.handleNewDonation}
+                donatedAmount={donatedAmount}
+
+                changeDonatedAmount={this.handleChangeDonatedAmount}
+                handleSubmit={this.handleSubmit}
+                donorName={donorName}
+                handleDonorName={this.handleDonorName}
+
+                donorCaption={donorCaption}
+                handleDonorCaption={this.handleDonorCaption}
+              />
+
+            </div>
           </div>
 
         </div>
 
       </div>
 
-    </div>
-  );
+
+    );
+  }
 }
 
 export default App;
