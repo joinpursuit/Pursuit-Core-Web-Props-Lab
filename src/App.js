b9b7356e2ddecf2f8d5c8b4f5f75f89abd3e3835
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-12-09 13:15:20 -0500

    0.1.1, reinstall react from scratch

---
+++ src/App.js
@@ -0,0 +1,23 @@
+/*
+Joseph P. Pasaoa
+Go Fund Me Lab | Joseph P. Pasaoa
+*/
+
+
+/* IMPORTS */
+import React from 'react';
+import './App.css';
+
+
+/* MAIN */
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+
+/* EXPORTS */
+export default App;
