c44acfd618422ed9d7c62a1bd4582af0d6a4c95d
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-12-10 13:57:51 -0500

    0.4, implemented data origin, props feed, fx feed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ APP | Go Fund Me Lab
 
 
 /* IMPORTS */
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import TopBar from './components/TopBar';
@@ -15,17 +15,51 @@ import DonationForm from './components/DonationForm';
 
 
 /* MAIN */
-function App() {
-  return (
-    <div className="App">
-      <div id="grid-base">
-        <TopBar />
-        <RecentDonations />
-        <Progress />
-        <DonationForm />
+class App extends Component {
+  constructor() {
+    super();
+    this.target = 1000;
+    this.state = {
+      donations: [
+        { donor: "Joey", amount: 211, msg: "Good luck, Alejo! May your trip be all you imagined and more!" },
+        { donor: "JR", amount: 400, msg: "wish I were the one going!!!" },
+        { donor: "Wynter", amount: 50, msg: "Bon voyage!" },
+        { donor: "Dessa", amount: 100, msg: "Bring me back something cool~" }
+      ]
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  handleChange = (e) => {
+    this.setState({
+        [e.target.name]: e.target.value
+    });
+  }
+
+  render() {
+    const { target, donations } = this.state;
+    return (
+      <div className="App">
+        <div id="grid-base">
+          <TopBar />
+          <RecentDonations 
+            donations={donations}
+          />
+          <Progress 
+            target={target} 
+            donations={donations}
+          />
+          <DonationForm 
+            handleSubmit={this.handleSubmit} 
+            handleChange={this.handleChange} 
+          />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 
