5ae6cde3cb7b773554189e379f1f9f8632f35c28
Jenesh <jenesh.napit@gmail.com>
2020-01-24 14:57:20 -0500

    Changed to hooks

--- go-fund-me-app/src/App.js
+++ go-fund-me-app/src/App.js
@@ -1,55 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import Feed from "./Components/Feed";
 import Donation from "./Components/Donation";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      donations: [
-        // { name: "John", amount: 25, message: "Kiki", id: 1 },
-        // { name: "Steve", amount: 50, message: "do you", id: 2 },
-        // { name: "Samsung", amount: 100, message: "luv me", id: 3 }
-      ],
-      donationTotal: 0,
-      donationGoal: 1000
-    };
-  }
+const App = () => {
+  const [state, setState] = useState({
+    donations: [
+      // { name: "John", amount: 25, message: "Kiki", id: 1 },
+      // { name: "Steve", amount: 50, message: "do you", id: 2 },
+      // { name: "Samsung", amount: 100, message: "luv me", id: 3 }
+    ],
+    donationTotal: 0,
+    donationGoal: 1000
+  })
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
   };
 
-  addDonation = props => {
-    const id = this.state.donations.length + 1;
+  const addDonation = props => {
+    const id = state.donations.length + 1;
     props.id = id;
-    const donationTotal =
-      this.state.donationTotal + Number(props.donationAmount);
-    this.setState({
-      ...this.state,
-      donations: [...this.state.donations, props],
+    const donationTotal = state.donationTotal + Number(props.donationAmount);
+    setState({
+      ...state,
+      donations: [...state.donations, props],
       donationTotal
     });
     console.log(props);
   };
 
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Feed donations={this.state.donations} />
-        <Donation
-          donationTotal={this.state.donationTotal}
-          donationGoal={this.state.donationGoal}
-          donationAmount={this.state.donationAmount}
-          handleSubmit={this.handleSubmit}
-          addDonation={this.addDonation}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Header />
+      <Feed donations={state.donations} />
+      <Donation
+        donationTotal={state.donationTotal}
+        donationGoal={state.donationGoal}
+        donationAmount={state.donationAmount}
+        handleSubmit={handleSubmit}
+        addDonation={addDonation}
+      />
+    </div>
+  );
 }
 
 export default App;
--- go-fund-me-app/src/Components/Donation.js
+++ go-fund-me-app/src/Components/Donation.js
@@ -1,66 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
-class Donation extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      donationAmount: 50,
-      message: "Good Luck!"
-    };
-  }
+const Donation = (props) => {
+  const [state, setState] = useState({
+    donationAmount: 50,
+    message: "Good Luck!"
+  })
 
-  handleChange = e => {
-    this.setState({ ...this.state, [e.target.name]: e.target.value });
+  const handleChange = e => {
+    setState({ ...state, [e.target.name]: e.target.value });
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    this.props.addDonation(this.state);
+    props.addDonation(state);
   };
 
-  render() {
-    return (
-      <div>
-        <h2 id="raised-display">
-          Raised ${this.props.donationTotal} of{" "}
-          <span id="donation-goal font-grey">${this.props.donationGoal}</span>
-        </h2>
-        <div id="donation-progress-bar">
-          {((this.props.donationTotal / this.props.donationGoal) * 100).toFixed(
-            2
-          )}
-          %
+  return (
+    <div>
+      <h2 id="raised-display">
+        Raised ${props.donationTotal} of{" "}
+        <span id="donation-goal font-grey">${props.donationGoal}</span>
+      </h2>
+      <div id="donation-progress-bar">
+        {((props.donationTotal / props.donationGoal) * 100).toFixed(
+          2
+        )}
+        %
         </div>
-        <form onSubmit={this.handleSubmit}>
-          <label name="name">Name</label>
-          <input
-            type="text"
-            name="name"
-            placeholder="John Doe"
-            onChange={this.handleChange}
-            required
-          />
-          <label name="message">Message</label>
-          <input
-            type="text"
-            name="message"
-            placeholder="Good Luck!"
-            onChange={this.handleChange}
-          />
-          <input
-            type="range"
-            name="donationAmount"
-            min="5"
-            max="100"
-            value={this.donationAmount}
-            onChange={this.handleChange}
-          />
-          <p>${this.state.donationAmount}</p>
-          <input type="submit" value="Donate" />
-        </form>
-      </div>
-    );
-  }
+      <form onSubmit={handleSubmit}>
+        <label name="name">Name</label>
+        <input
+          type="text"
+          name="name"
+          placeholder="John Doe"
+          onChange={handleChange}
+          required
+        />
+        <label name="message">Message</label>
+        <input
+          type="text"
+          name="message"
+          placeholder="Good Luck!"
+          onChange={handleChange}
+        />
+        <input
+          type="range"
+          name="donationAmount"
+          min="5"
+          max="100"
+          value={state.onationAmount}
+          onChange={handleChange}
+        />
+        <p>${state.donationAmount}</p>
+        <input type="submit" value="Donate" />
+      </form>
+    </div>
+  );
 }
 
 export default Donation;
