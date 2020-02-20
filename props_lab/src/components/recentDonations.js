038a58efb3c5f0c9bd33bc2f885fe424189dfe4b
ashya <ashyamanning@pursuit.org>
2020-02-19 21:59:50 -0500

    handling submit

--- props_lab/src/components/recentDonations.js
+++ props_lab/src/components/recentDonations.js
@@ -1,28 +1,13 @@
 import React from 'react';
 import DonationDisplay from './donationDisplay';
 
-class RecentDonations extends React.Component {
-    state = {
-        donations: [
-            {name: "",
-            amount: 0,
-            caption: ""}
-        ]
-    }
-    
-    render() {
-        let donations = this.state.donations.map((donation, i) => {
-            return <DonationDisplay key={i}
-            name={donation.name}
-            amount={donation.amount}
-            caption={donation.caption} />
-        })
-        return(
-            <div>
-                {donations}
-            </div>
-        )
-    }
-}
+const RecentDonations = ((props, i) => {
+    return(
+        <DonationDisplay key={i}
+        name={props.name}
+        amount={props.amount}
+        caption={props.caption} />
+    )
+})
 
 export default RecentDonations;
