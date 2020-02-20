a3075685672deef2a004fc096c4c1863c8f617a2
crystal cardona <crystalcardona@pursuit.org>
2020-02-19 20:12:53 -0500

    working on DonationsDisplay

--- go-fund-me/src/App.js
+++ go-fund-me/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import TopBar from './components/TopBar'
+import TopBar from './components/TopBar';
+import DonationDisplay from './components/DonationDisplay'
 
 function App() {
   return (
     <div className="App">
     <TopBar /> 
+    <DonationDisplay />
     </div>
   );
 }
