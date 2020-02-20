d27bfcfdc4a3241bd35b0f3a3cf00fe950419b37
maria liza <mariamartinez@pursuit.org>
2020-02-19 20:05:52 -0500

    add functionality to form

--- go_fund_me/src/components/donationForm.js
+++ go_fund_me/src/components/donationForm.js
@@ -4,12 +4,34 @@ class DonationForm extends React.Component {
   state = {
     name: "",
     caption: "",
-    donation: 0
+    donation: 0,
+    donationList: []
   };
 
   handleSubmit = e => {
-    const { name, caption, donation } = this.state;
+    const { name, caption, donation, donationList } = this.state;
     e.preventDefault();
+    this.setState(prevState => {
+      return {
+        donationList: [
+          ...prevState.donationList,
+          {
+            name: name,
+            caption: caption,
+            donation: donation
+          }
+        ]
+      };
+    });
+    console.log(donationList);
+    // debugger;
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+    console.log(this.state);
   };
 
   render() {
@@ -20,7 +42,9 @@ class DonationForm extends React.Component {
           <input
             type="text"
             value={this.state.name}
+            name="name"
             onChange={this.handleChange}
+            required
           />
         </label>
 
@@ -29,7 +53,9 @@ class DonationForm extends React.Component {
           <input
             type="text"
             value={this.state.caption}
+            name="caption"
             onChange={this.handleChange}
+            required
           />
         </label>
 
@@ -40,7 +66,10 @@ class DonationForm extends React.Component {
             value={this.state.donation}
             min="0"
             onChange={this.handleChange}
+            name="donation"
           />
+          {this.state.donation}
+          <input type="submit" value="donate" />
         </lable>
       </form>
     );
