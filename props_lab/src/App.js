0105b896ae1bc458e533a26235d77e96fec79660
Kevin Wong <kevinwong@pursuit.org>
2020-02-19 22:01:58 -0500

    work on displaying recent donors...

--- props_lab/src/App.js
+++ props_lab/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 import TopBar from "./components/TopBar";
+import DisplayRecentDonations from "./components/RecentDonations";
 import DonationForm from "./components/DonationForm";
 
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <DisplayRecentDonations />
       <DonationForm />
     </div>
   );
---
+++ props_lab/src/components/RecentDonations.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DisplayRecentDonations = () => {
+  return (
+    <>
+      <h3>Recent Donations</h3>
+      <ul></ul>
+    </>
+  );
+};
+
+export default DisplayRecentDonations;
