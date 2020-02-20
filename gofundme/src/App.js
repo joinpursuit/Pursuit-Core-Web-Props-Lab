f17f989d4b1f0b8482c981317388182f1493cec8
Henrynunez112 <henrynunez@pursuit.org>
2020-02-19 22:03:39 -0500

    almost done

--- gofundme/src/App.js
+++ gofundme/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
-import Donations from './component/Form'
+import Donations from './component/Donations'
 
 function App() {
   return (
