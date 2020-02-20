a6e490d56dd920d5cc41b860b9bf66125b03b0b5
Ohidurr <Ohidurrahman96@gmail.com>
2020-02-19 19:35:54 -0500

    create react app, and top bar

---
+++ props_lab/src/components/topBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TopBar = () => {
+    return (
+       <section className="topBar">
+           <h1 className="topBarHeading">Go Fund Me</h1>
+           <p className='topBarText'> Help me get a job</p>
+       </section>
+    )
+}
+
+
+export default TopBar;
