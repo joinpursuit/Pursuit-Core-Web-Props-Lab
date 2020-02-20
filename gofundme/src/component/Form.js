e823d91bd9c6087490b1b950a9bdfc5f43c9c5c6
Henrynunez112 <henrynunez@pursuit.org>
2020-02-19 20:45:54 -0500

    finished forms

--- gofundme/src/component/Form.js
+++ gofundme/src/component/Form.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 class Donations extends React.Component{
     state = {
+        name: '',
+        captions: '',
         recentDonations: [],
         value: 0,
         raised: 0,
@@ -13,32 +15,53 @@ class Donations extends React.Component{
             value: e.target.value
         })
     }
-    handleGoal =()=>{
-   
+    handleName =(e)=>{
+        this.setState({
+            name: e.target.value,
+        })
+    }
+    handleCaption =(e)=>{
+        this.setState({
+            captions: e.target.value
+        })
+    }
+
+    handleButton = (e)=>{
+        e.preventDefault();
+        this.setState({
+            recentDonations: [{
+                name: this.state.name,
+                captions: this.state.captions,
+                amount: this.state.value
+            }]
+        })
     }
 
 
 
     render(){
-        console.log(this.state.value)
+        console.log(this.state.recentDonations)
         const {value, raised, goal} = this.state
         return(
-            <form>
+            <form onSubmit={this.handleButton}>
                 <h2>Raised ${raised} of ${goal}</h2>
+
                 <label>
                 Name:
-                <input type='text' placeholder="Name"></input>
+                <input type='text' placeholder="Name" onChange={this.handleName}></input>
                 </label>
                 <label>
                 Caption:
-                <input type='text' placeholder="Caption"></input>
+                <input type='text' placeholder="Caption" onChange={this.handleCaption}></input>
                 </label>
-                <label for="points">Amount to Donate</label>
+                <label className="points">Amount to Donate</label>
                 <input type="range" id="points" name="points" min="0" max="100" data-show-value ="true" onChange={this.handleValue}></input>
                 <p>${value}</p>
+
+                    <input type='submit' ></input>
             </form>
         )
     }
 }
 
-export default Donations
+export default Donations;
