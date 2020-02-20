10033dde823a0a250e34e590b5abe2a0c71afc97
BrandonBrown <brandonbrown@pursuit.org>
2020-02-19 19:37:29 -0500

    add top bar

---
+++ propslab/src/components/topBar/topBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TopBar = (props) => {
+return (
+    <section class="jumbotron text-center">
+    <h1 class="jumbotron-heading">Go Fund Me</h1>
+    <p class="lead text-muted">Help me go on a vacation to a beach somewhere</p>
+    </section>
+)
+}
+
+export default TopBar;
