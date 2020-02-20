4ac9e60851cbb07776bdf87c00ffe7990b8f2359
Ohidurr <Ohidurrahman96@gmail.com>
2020-02-19 20:49:59 -0500

    donation range working

--- props_lab/src/components/form.js
+++ props_lab/src/components/form.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Form = () => {
+class Form extends Component {
+     
+    state = {
+        amount: "5"
+    }
+    handleChange = (e) => {
+        // debugger
+        this.setState({ 
+            [e.target.name]: e.target.value
+            
+            
+            
+        })
+    render(){
+        console.log(this.state)
     return(
         <div>
     <form className="dontationForm">
@@ -11,17 +25,18 @@ const Form = () => {
         Caption: <input className="caption" placeholder="..."/>
         </div>
         <div>
-        Amount to Donate: <input type="range" className="donationAmount" mins="5" max="1000"/>
+        Amount to Donate: <input  name="amount" type="range" className="donationAmount" mins="5" max="1000" onChange={this.handleChange}/>
         <blockquote className="blockquote">
-            <p className="amount"> $ 5 </p>
+            <p name="amount" value={this.state.amount} className="displayAmount" >{ this.state.amount}</p>
             <button className="button">Donate</button>
+            {this.testFunc}
         </blockquote>
+       
         </div>
-        
-    
-    </form>
+        </form>
     </div>
     )
+    }
 }
 
 
