242aa212f101bf563547240d9cb5b77fb960cf48
Kevin Brutus <kevinbrutus@pursuit.org>
2020-02-19 19:19:33 -0500

    create recentdonations class. linked to app.js

--- go-fundme/src/App.js
+++ go-fundme/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 import TopBar from './components/TopBar';
+import RecentDonations from './components/RecentDonations';
 
 const App = () => {
   return (
     <div className="App">
       <TopBar />
+      <RecentDonations />
     </div>
   );
 }
---
+++ go-fundme/src/components/RecentDonations.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class RecentDonations extends React.Component{
+    state = {
+        donations: []
+    }
+    
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default RecentDonations;
