16c23190285f0ec2c4fdf0f37a01dc9b74886d55
Kelvin <kelvinarellano@pursuit.org>
2020-02-19 21:45:34 -0500

    build initial frame work

---
+++ go-fund-me/src/components/donationDisplay.js
@@ -0,0 +1,10 @@
+import React from "react"
+const DonationDisplay = ({}) => {
+    return(
+        <div className="donationContainer">
+            <h4>{name}</h4>
+            <div>{amount}</div>
+
+        </div>
+    )
+}
