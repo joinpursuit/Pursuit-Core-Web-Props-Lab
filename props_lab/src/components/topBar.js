21f1e5478d320d3c8edfe079ef187a8eade904e7
ashya <ashyamanning@pursuit.org>
2020-02-19 19:32:05 -0500

    set up TopBar.js

---
+++ props_lab/src/components/topBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TopBar = () => {
+    return(
+        <>
+            <div>
+                <h1>goFund Mo</h1>
+                <h3>Help me go on vacation to a beach somewhere</h3>
+            </div>
+        </>
+    )
+}
+
+export default TopBar;
